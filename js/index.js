113e55eaeb2560770e2b6508ae1b0cfe429252c7
Sharif <mdsh8692@gmail.com>
2024-09-23 15:39:48 +0600

    input validation complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-console.log('is connected?');
+let handleTotalTrancsaction = function () {
+  const donationInput = parseFloat(document.getElementById('donation-input').value);
+  let donatedAmount = parseFloat(document.getElementById("donated-amount").innerText);
+  let accountBalance = parseFloat(document.getElementById("account-balance").innerText);
+  if(donationInput < 0 || donationInput > accountBalance){
+    return alert('Invalid amount number')
+  }
+  else if(isNaN(donationInput)){
+    return alert('Write valid number')
+  }
+
+   else {
+    document.getElementById('donated-amount').innerText = donationInput + donatedAmount
+    let totalAccountBal = accountBalance - donationInput
+    document.getElementById('account-balance').innerText = totalAccountBal
+ 
+  
+
+
+  
+ 
+
+    
+   }
+
+  
+}
+
+document.getElementById("donate-btn").addEventListener("click", handleTotalTrancsaction);
